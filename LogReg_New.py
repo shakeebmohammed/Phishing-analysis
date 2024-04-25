830518783ef79b77f512814a023d9604a4831616
shakeebmohammed <138548563+shakeebmohammed@users.noreply.github.com>
2024-04-25 13:54:04 -0500

    Add files via upload

---
+++ LogReg_New.py
@@ -0,0 +1,46 @@
+
+import utils
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn import metrics
+import shap
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+
+df=pd.read_csv('URLs.csv')
+
+# Defining Training and Testing Set for Classification
+
+X= df[['Have_IP', 'Have_At', 'URL_Length', 'URL_Depth', 'Redirection',
+       'https_Domain', 'TinyURL', 'Prefix/Suffix']]
+
+y=df['Label']
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state = 42)
+
+# Logistic Regression
+logreg=LogisticRegression(random_state=42)
+logreg.fit(X_train,y_train)
+y_pred = logreg.predict(X_test)
+
+cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
+cnf_matrix
+
+target_names = ['Not phishing', 'Phishing']
+print(classification_report(y_test, y_pred, target_names=target_names))
+
+# Calculate ROC curve and AUC
+fpr, tpr, _ = metrics.roc_curve(y_test, y_pred, pos_label=1)
+auc = metrics.roc_auc_score(y_test, y_pred)
+
+# Plot ROC curve
+# Convert AUC value to string before concatenating
+plt.plot(fpr, tpr, label="data 1, auc=" + str(auc))
+
+plt.legend(loc=4)
+plt.show()
---
+++ LogReg_Old.py
@@ -0,0 +1,53 @@
+
+import utils
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn import metrics
+import shap
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+import openpyxl
+
+df=pd.read_excel('phising-uci-dataset.xlsx')
+
+# Defining Training and Testing Set for Classification
+
+X= df[['having_IP_Address', 'URL_Length', 'Shortining_Service',
+       'having_At_Symbol', 'double_slash_redirecting', 'Prefix_Suffix',
+       'having_Sub_Domain', 'SSLfinal_State', 'Domain_registeration_length',
+       'Favicon', 'port', 'HTTPS_token', 'Request_URL', 'URL_of_Anchor',
+       'Links_in_tags', 'SFH', 'Submitting_to_email', 'Abnormal_URL',
+       'Redirect', 'on_mouseover', 'RightClick', 'popUpWidnow', 'Iframe',
+       'age_of_domain', 'DNSRecord', 'web_traffic', 'Page_Rank',
+       'Google_Index', 'Links_pointing_to_page', 'Statistical_report']]
+
+y=df['Result']
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state = 42)
+
+# Logistic Regression
+logreg=LogisticRegression(random_state=42)
+logreg.fit(X_train,y_train)
+y_pred = logreg.predict(X_test)
+
+cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
+cnf_matrix
+
+target_names = ['Not phishing', 'Phishing']
+print(classification_report(y_test, y_pred, target_names=target_names))
+
+# Calculate ROC curve and AUC
+fpr, tpr, _ = metrics.roc_curve(y_test, y_pred, pos_label=1)
+auc = metrics.roc_auc_score(y_test, y_pred)
+
+# Plot ROC curve
+# Convert AUC value to string before concatenating
+plt.plot(fpr, tpr, label="data 1, auc=" + str(auc))
+
+plt.legend(loc=4)
+plt.show()
